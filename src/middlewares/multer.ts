85a9143b3c1b0ed998fb140b5f9599a4ac24bae8
Abhi8099 <abhishek.sharma1@digranknow.com>
2025-05-30 13:13:16 +0530

    fix upload agn

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,24 +1,16 @@
 import multer from 'multer';
 import { v4 as uuid } from 'uuid';
-import fs from 'fs';
-import path from 'path';
-
-const uploadDir = path.join(__dirname, 'uploads');
-
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, uploadDir);
+        cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
-        const id = uuid()
+        const id =uuid()
         const extName = file.originalname.split('.').pop();
         const fileName = `${id}.${extName}`
-        cb(null, fileName);
+        cb(null,fileName);
     }
 });
 
-export const singleUpload = multer({ storage }).single("photo")
+export const singleUpload = multer({storage}).single("photo")
