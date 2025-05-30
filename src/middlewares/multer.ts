d55e8e68495d8c7c471423e057f84bd1d32c98ee
Abhi8099 <abhishek.sharma1@digranknow.com>
2025-05-30 13:00:56 +0530

    fix upload agn

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -2,6 +2,10 @@ import multer from 'multer';
 import { v4 as uuid } from 'uuid';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const uploadDir = path.join(__dirname, 'uploads');
 
